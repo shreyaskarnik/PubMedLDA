6d0138a64ca003b07dd2b227d0b093f62abab49d
Shreyas Karnik <sdkarnik@iupui.edu>
2011-09-19 13:16:02 -0400

    Minor Change

--- getpmAbstracts.py
+++ getpmAbstracts.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 getpmAbstracts.py
+Retrieving PubMed abstracts
 Author: Shreyas Karnik
 """
 import urllib
@@ -23,10 +24,10 @@ parser.add_option("-s", "--stem",action="store_true",dest="stem",help="To stem r
 (options, args) = parser.parse_args()
 
 if (len(options.query)<0):
-	parser.print_help()
+    parser.print_help()
 
 if (len(options.outfile)<0):
-        parser.print_help()
+    parser.print_help()
 
 
 ######functions
@@ -37,47 +38,47 @@ def de_safe_xml(kinda_xml):
     """Converts an escaped HTML/XML into a more normal string."""
     htmlCodes = (('&', '&amp;'),('<', '&lt;'),('>', '&gt;'),('"', '&quot;'),("'", '&#39;'))
     for rep, orig in htmlCodes:
-     	kinda_xml = kinda_xml.replace(orig, rep)
+        kinda_xml = kinda_xml.replace(orig, rep)
     return kinda_xml
 
 def main():
-	query=options.query
-	ofile=options.outfile
-	esearch = 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&retmode=xml&retmax=10000000&term=%s' % (query)
-	handle = urllib.urlopen(esearch)
-	data = handle.read()
-	root = ET.fromstring(data)
-	f = open(ofile, 'a')
-	ids = [x.text for x in root.findall("IdList/Id")]
-	print 'Got %d articles' % (len(ids))
-	count=0
-	for group in chunker(ids, 100):
-		efetch = "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?&db=pubmed&retmode=xml&id=%s" % (','.join(group))
-    		handle = urllib.urlopen(efetch)
-    		data = handle.read()
-		root = ET.fromstring(data)
-		for article in root.findall("PubmedArticle"):
-			abstract_text=''
-			pmid = article.find("MedlineCitation/PMID").text
-        		title = article.findtext("MedlineCitation/Article/ArticleTitle")
-			abstract = article.findall("MedlineCitation/Article/Abstract/AbstractText")
-			#print (len(abstract))
-			if(len(abstract)>0):
-				count=count+1
-				for abst_part in abstract:
-					abstract_text+=' '+str(abst_part.text)
-				title.encode('utf-8', 'replace')
-				abstract_text.encode('utf-8', 'replace')     		
-				final=str(title)+" "+str(abstract_text)
-				final.encode('utf-8', 'replace')
-			        if(options.stem==True):
-					final=final.translate(None,string.punctuation)
-					f.write(stem_text(final.lower())+"\n")
-				else:
-				  final=final.translate(None,string.punctuation)
-				  f.write(final+"\n")
-			else:
-			   print 'skipping PMID %s as length of abstract is zero' % pmid
-	print 'written to %s articles to file %s' % (count,ofile)
+    query=options.query
+    ofile=options.outfile
+    esearch = 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&retmode=xml&retmax=10000000&term=%s' % (query)
+    handle = urllib.urlopen(esearch)
+    data = handle.read()
+    root = ET.fromstring(data)
+    f = open(ofile, 'a')
+    ids = [x.text for x in root.findall("IdList/Id")]
+    print 'Got %d articles' % (len(ids))
+    count=0
+    for group in chunker(ids, 100):
+        efetch = "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?&db=pubmed&retmode=xml&id=%s" % (','.join(group))
+        handle = urllib.urlopen(efetch)
+        data = handle.read()
+        root = ET.fromstring(data)
+        for article in root.findall("PubmedArticle"):
+            abstract_text=''
+            pmid = article.find("MedlineCitation/PMID").text
+            title = article.findtext("MedlineCitation/Article/ArticleTitle")
+            abstract = article.findall("MedlineCitation/Article/Abstract/AbstractText")
+            #print (len(abstract))
+            if(len(abstract)>0):
+                count=count+1
+                for abst_part in abstract:
+                    abstract_text+=' '+str(abst_part.text)
+                title.encode('utf-8', 'replace')
+                abstract_text.encode('utf-8', 'replace')     		
+                final=str(title)+" "+str(abstract_text)
+                final.encode('utf-8', 'replace')
+                if(options.stem==True):
+                    final=final.translate(None,string.punctuation)
+                    f.write(stem_text(final.lower())+"\n")
+                else:
+                    final=final.translate(None,string.punctuation)
+                f.write(final+"\n")
+            else:
+                print 'skipping PMID %s as length of abstract is zero' % pmid
+    print 'written to %s articles to file %s' % (count,ofile)
 if __name__ == "__main__":
     main()
