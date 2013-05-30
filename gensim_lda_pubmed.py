626afa7eea99412cc2f85f32880598f974b503a5
Shreyas <shreyas@zendeals.com>
2013-05-29 18:09:11 -0700

    PEP8 compliant code

--- gensim_lda_pubmed.py
+++ gensim_lda_pubmed.py
@@ -5,98 +5,92 @@ gensim_lda_pubmed.py
 Created by Shreyas Karnik on 2011-07-02.
 """
 import logging
-from gensim import corpora, models, similarities
+from gensim import corpora, models
 from nltk.corpus import stopwords
 from optparse import OptionParser
-import string
-from nltk.stem.porter import *
-from gensim.parsing import stem_text
 
-"""
-Some options using the option parser 
-"""
 usage = "usage: python %prog -i [inputfile] -k [number of topics to extract] -v [verbose output FALSE by default] -t [TRUE/ FALSE for TFIDF weights] -r [return topics per document TRUE/FALSE (default FALSE)]"
 parser = OptionParser(usage=usage)
-parser.add_option("-i", "--inputfile",action="store", dest="inputfile",help="Enter the file containing PubMed abstracts", metavar="IFILE")
-parser.add_option("-k", "--numtopics",action="store",dest="ntopics",type="int",help="Number of topics", metavar="NTOP")
-parser.add_option("-t", "--tfidf",action="store",dest="tfidf",help="TFIDF weignting (default TRUE)", metavar="TFIDF",default="TRUE")
-parser.add_option("-v",action="store",help="Verbose Output TRUE/FALSE (default FALSE)", dest="verbose",default="FALSE")
-parser.add_option("-r",action="store",help="Return topics per document TRUE/FALSE (default FALSE)", dest="fit",default="FALSE")
-(options, args) = parser.parse_args()
-if (len(options.inputfile)<0):
-	parser.print_help()
-if (options.ntopics<=0):
-        parser.print_help()
+parser.add_option("-i", "--inputfile", action="store", dest="inputfile", help="Enter the file containing PubMed abstracts", metavar="IFILE")
+parser.add_option("-k", "--numtopics", action="store", dest="ntopics", type="int", help="Number of topics", metavar="NTOP")
+parser.add_option("-t", "--tfidf", action="store", dest="tfidf", help="TFIDF weignting (default TRUE)", metavar="TFIDF", default="TRUE")
+parser.add_option("-v", action="store", help="Verbose Output TRUE/FALSE (default FALSE)", dest="verbose", default="FALSE")
+parser.add_option("-r", action="store", help="Return topics per document TRUE/FALSE (default FALSE)", dest="fit", default="FALSE")
+options, args = parser.parse_args()
+if len(options.inputfile) < 0:
+    parser.print_help()
+if options.ntopics <= 0:
+    parser.print_help()
+
+
 def main():
- 
-  model=options.inputfile+"_lda.model"
-  inputfile=options.inputfile
-  dicto=options.inputfile+"_lda.dict"
-  ntopics=options.ntopics
-  corpname=options.inputfile+"_lda.corpus"
-  stoplist = stopwords.words('english')
-  if(options.verbose=="TRUE"):
-    logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
-  else:
-    logger = logging.getLogger()
-    formatter =logging.Formatter('%(asctime)s : %(levelname)s : %(message)s')
-    log_file_name=inputfile+"_lda.log"
-    hdlr = logging.FileHandler(log_file_name)
-    hdlr.setFormatter(formatter)
-    logger.addHandler(hdlr) 
-    logger.setLevel(logging.INFO)
-  
-  """
-  Starting the number crunching
-  """
-  """
-  Creating dictionary while removing stopwords and words that occur only once.
-  """
-  
-  texts = [[word for word in document.lower().split() if word not in stoplist] for document in open(inputfile)]
-  #texts = [[word.translate(None,string.punctuation) for word in document.lower().split() if word not in stoplist] for document in open(inputfile)]
-  print ("Reading and tokenizing %s") % (inputfile)
-  dictionary = corpora.Dictionary(line.lower().split() for line in open(inputfile))
-  #dictionary = corpora.Dictionary(line.lower().split() for line in open(inputfile))
-  stop_ids = [dictionary.token2id[stopword] for stopword in stoplist if stopword in dictionary.token2id]
-  #removing stopwords
-  once_ids = [tokenid for tokenid, docfreq in dictionary.dfs.iteritems() if docfreq == 1]
-  dictionary.filter_tokens(stop_ids + once_ids)
-  dictionary.compactify()
-  print dictionary
-  """
-  Dictionary created and stored for future reference
-  """
-  dictionary.save(dicto)
-  """
-  Creating corpus and storing to disk, for later use
-  using tfidf weights
-  """
-  corpus = [dictionary.doc2bow(text) for text in texts]
-  if(options.tfidf=="TRUE"):
-    tfidf = models.TfidfModel(corpus)
-    corpus = tfidf[corpus]
-    corpora.MmCorpus.serialize(corpname, corpus)
-  else:
-    corpora.MmCorpus.serialize(corpname, corpus) 
-  """
-  Running LDA
-  """
-  lda =models.LdaModel(corpus=corpus, id2word=dictionary, num_topics=ntopics,passes=40,update_every=1,chunksize=800)
-  """
-  Printing LDA output
-  """
-  print("LDA finished printing %d topics with 12 top words") %(ntopics)
-  lda.print_topics(topics=ntopics,topn=12)
-  lda.save(model)                                                                                    
-  if(options.fit=="TRUE"):
-    out_fit_file=options.inputfile+"_lda.tc"
-    print "Topic allocation written in %s" % (out_fit_file)
-    f=open(out_fit_file,"a")
-    doc_lda = lda[corpus]
-    for doc in doc_lda:
-      f.write(str(doc)+"\n")  
-  if(options.verbose=="FALSE"):
-    print "Processing finished please check %s for details" %(log_file_name) 
+    model = options.inputfile + "_lda.model"
+    inputfile = options.inputfile
+    dicto = options.inputfile + "_lda.dict"
+    ntopics = options.ntopics
+    corpname = options.inputfile + "_lda.corpus"
+    stoplist = stopwords.words('english')
+    if options.verbose == "TRUE":
+        logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+    else:
+        logger = logging.getLogger()
+        formatter = logging.Formatter('%(asctime)s : %(levelname)s : %(message)s')
+        log_file_name = inputfile + "_lda.log"
+        hdlr = logging.FileHandler(log_file_name)
+        hdlr.setFormatter(formatter)
+        logger.addHandler(hdlr)
+        logger.setLevel(logging.INFO)
+
+    """
+    Starting the number crunching
+    """
+    """
+    Creating dictionary while removing stopwords and words that occur only once.
+    """
+
+    texts = [[word for word in document.lower().split() if word not in stoplist] for document in open(inputfile)]
+    logging.info("Reading and tokenizing {}".format(inputfile))
+    dictionary = corpora.Dictionary(line.lower().split() for line in open(inputfile))
+    stop_ids = [dictionary.token2id[stopword] for stopword in stoplist if stopword in dictionary.token2id]
+    #removing stopwords
+    once_ids = [tokenid for tokenid, docfreq in dictionary.dfs.iteritems() if docfreq == 1]
+    dictionary.filter_tokens(stop_ids + once_ids)
+    dictionary.compactify()
+    """
+    Dictionary created and stored for future reference
+    """
+    dictionary.save(dicto)
+    """
+    Creating corpus and storing to disk, for later use
+    using tfidf weights
+    """
+    corpus = [dictionary.doc2bow(text) for text in texts]
+    if options.tfidf == "TRUE":
+        tfidf = models.TfidfModel(corpus)
+        corpus = tfidf[corpus]
+        corpora.MmCorpus.serialize(corpname, corpus)
+    else:
+        corpora.MmCorpus.serialize(corpname, corpus)
+    """
+    Running LDA
+    """
+    lda = models.LdaModel(corpus=corpus, id2word=dictionary, num_topics=ntopics, passes=40, update_every=1, chunksize=800)
+    """
+    Printing LDA output
+    """
+    logging.info("LDA finished printing {} topics with 12 top words".format(ntopics))
+    lda.print_topics(topics=ntopics, topn=12)
+    lda.save(model)
+    if options.fit:
+        out_fit_file = options.inputfile + "_lda.tc"
+        logging.info("Topic allocation written in {}".format(out_fit_file))
+        f = open(out_fit_file, "a")
+        doc_lda = lda[corpus]
+        for doc in doc_lda:
+            f.write(str(doc) + "\n")
+    if not options.verbose:
+        logging.info("Processing finished please check {} for details".format(log_file_name))
+
+
 if __name__ == "__main__":
-  main()
+    main()
--- getpmAbstracts.py
+++ getpmAbstracts.py
@@ -6,27 +6,28 @@ Retrieving PubMed abstracts
 Author: Shreyas Karnik
 """
 import urllib
-import urllib2
 import sys
 from optparse import OptionParser
 import xml.etree.ElementTree as ET
 import logging
+
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 from gensim.parsing import stem_text
 import string
+
 reload(sys)
 sys.setdefaultencoding('utf-8')
 usage = "usage:python %prog -q [query] -o [output] -s [flag for steming]"
 parser = OptionParser(usage=usage)
-parser.add_option("-q", "--query",action="store", dest="query",help="Enter the PubMed query (PubMed style queries supported)", metavar="QUERY")
-parser.add_option("-o", "--output",action="store",dest="outfile",help="Enter the output file name to store result", metavar="OFILE")
-parser.add_option("-s", "--stem",action="store_true",dest="stem",help="To stem result", metavar="STEM")
+parser.add_option("-q", "--query", action="store", dest="query", help="Enter the PubMed query (PubMed style queries supported)", metavar="QUERY")
+parser.add_option("-o", "--output", action="store", dest="outfile", help="Enter the output file name to store result", metavar="OFILE")
+parser.add_option("-s", "--stem", action="store_true", dest="stem", help="To stem result", metavar="STEM")
 (options, args) = parser.parse_args()
 
-if (len(options.query)<0):
+if len(options.query) < 0:
     parser.print_help()
 
-if (len(options.outfile)<0):
+if len(options.outfile) < 0:
     parser.print_help()
 
 
@@ -34,51 +35,54 @@ if (len(options.outfile)<0):
 def chunker(seq, size):
     return (seq[pos:pos + size] for pos in xrange(0, len(seq), size))
 
+
 def de_safe_xml(kinda_xml):
     """Converts an escaped HTML/XML into a more normal string."""
-    htmlCodes = (('&', '&amp;'),('<', '&lt;'),('>', '&gt;'),('"', '&quot;'),("'", '&#39;'))
+    htmlCodes = (('&', '&amp;'), ('<', '&lt;'), ('>', '&gt;'), ('"', '&quot;'), ("'", '&#39;'))
     for rep, orig in htmlCodes:
         kinda_xml = kinda_xml.replace(orig, rep)
     return kinda_xml
 
+
 def main():
-    query=options.query
-    ofile=options.outfile
-    esearch = 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&retmode=xml&retmax=10000000&term=%s' % (query)
+    query = options.query
+    ofile = options.outfile
+    esearch = 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&retmode=xml&retmax=10000000&term=%s'.format(query)
     handle = urllib.urlopen(esearch)
     data = handle.read()
     root = ET.fromstring(data)
     f = open(ofile, 'a')
     ids = [x.text for x in root.findall("IdList/Id")]
-    print 'Got %d articles' % (len(ids))
-    count=0
+    logging.info('Got {} articles'.format(len(ids)))
+    count = 0
     for group in chunker(ids, 100):
-        efetch = "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?&db=pubmed&retmode=xml&id=%s" % (','.join(group))
+        efetch = "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?&db=pubmed&retmode=xml&id=%s".format((','.join(group)))
         handle = urllib.urlopen(efetch)
         data = handle.read()
         root = ET.fromstring(data)
         for article in root.findall("PubmedArticle"):
-            abstract_text=''
+            abstract_text = ''
             pmid = article.find("MedlineCitation/PMID").text
             title = article.findtext("MedlineCitation/Article/ArticleTitle")
             abstract = article.findall("MedlineCitation/Article/Abstract/AbstractText")
-            #print (len(abstract))
-            if(len(abstract)>0):
-                count=count+1
+            if len(abstract) > 0:
+                count += 1
                 for abst_part in abstract:
-                    abstract_text+=' '+str(abst_part.text)
+                    abstract_text += ' ' + str(abst_part.text)
                 title.encode('utf-8', 'replace')
-                abstract_text.encode('utf-8', 'replace')     		
-                final=str(title)+" "+str(abstract_text)
+                abstract_text.encode('utf-8', 'replace')
+                final = str(title) + " " + str(abstract_text)
                 final.encode('utf-8', 'replace')
-                if(options.stem==True):
-                    final=final.translate(None,string.punctuation)
-                    f.write(stem_text(final.lower())+"\n")
+                if options.stem:
+                    final = final.translate(None, string.punctuation)
+                    f.write(stem_text(final.lower()) + "\n")
                 else:
-                    final=final.translate(None,string.punctuation)
-                f.write(final+"\n")
+                    final = final.translate(None, string.punctuation)
+                f.write(final + "\n")
             else:
-                print 'skipping PMID %s as length of abstract is zero' % pmid
-    print 'written to %s articles to file %s' % (count,ofile)
+                logging.info('skipping PMID {} as length of abstract is zero'.format(pmid))
+    logging.info('written to {} articles to file {}'.format(count, ofile))
+
+
 if __name__ == "__main__":
     main()
