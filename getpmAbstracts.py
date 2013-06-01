0c9d5aab087d0c5f5a0c223fe6dc436dfaba2061
Shreyas <shreyas@zendeals.com>
2013-05-31 19:03:22 -0700

    using requests

--- getpmAbstracts.py
+++ getpmAbstracts.py
@@ -5,20 +5,20 @@ getpmAbstracts.py
 Retrieving PubMed abstracts
 Author: Shreyas Karnik
 """
-import urllib
+import requests
 import sys
-from optparse import OptionParser
+import optparse
 import xml.etree.ElementTree as ET
 import logging
-
-logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
-from gensim.parsing import stem_text
 import string
+import gensim.parsing
+logging.basicConfig(format='%(asctime)s %(levelname)-7s %(message)s [%(pathname)s]', level=logging.INFO)
 
 reload(sys)
 sys.setdefaultencoding('utf-8')
+
 usage = "usage:python %prog -q [query] -o [output] -s [flag for steming]"
-parser = OptionParser(usage=usage)
+parser = optparse.OptionParser(usage=usage)
 parser.add_option("-q", "--query", action="store", dest="query", help="Enter the PubMed query (PubMed style queries supported)", metavar="QUERY")
 parser.add_option("-o", "--output", action="store", dest="outfile", help="Enter the output file name to store result", metavar="OFILE")
 parser.add_option("-s", "--stem", action="store_true", dest="stem", help="To stem result", metavar="STEM")
@@ -47,19 +47,17 @@ def de_safe_xml(kinda_xml):
 def main():
     query = options.query
     ofile = options.outfile
-    esearch = 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&retmode=xml&retmax=10000000&term=%s'.format(query)
-    handle = urllib.urlopen(esearch)
-    data = handle.read()
-    root = ET.fromstring(data)
+    esearch = 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&retmode=xml&retmax=10000000&term={}'.format(query)
+    response = requests.get(esearch)
+    root = ET.fromstring(response.text)
     f = open(ofile, 'a')
     ids = [x.text for x in root.findall("IdList/Id")]
     logging.info('Got {} articles'.format(len(ids)))
     count = 0
     for group in chunker(ids, 100):
-        efetch = "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?&db=pubmed&retmode=xml&id=%s".format((','.join(group)))
-        handle = urllib.urlopen(efetch)
-        data = handle.read()
-        root = ET.fromstring(data)
+        efetch = "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?&db=pubmed&retmode=xml&id={}".format((','.join(group)))
+        response = requests.get(efetch)
+        root = ET.fromstring(response.text)
         for article in root.findall("PubmedArticle"):
             abstract_text = ''
             pmid = article.find("MedlineCitation/PMID").text
@@ -75,7 +73,7 @@ def main():
                 final.encode('utf-8', 'replace')
                 if options.stem:
                     final = final.translate(None, string.punctuation)
-                    f.write(stem_text(final.lower()) + "\n")
+                    f.write(gensim.parsing.stem_text(final.lower()) + "\n")
                 else:
                     final = final.translate(None, string.punctuation)
                 f.write(final + "\n")
